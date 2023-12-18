cc355b0a521a3974a3d932451289f80745fba824
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-12-18 23:26:10 +0000

    Working with numbers

---
+++ python_work/Chapter2/ex0209.py
@@ -0,0 +1,5 @@
+# Working with numbers #
+print(2 + 6)
+print(12 - 4)
+print(1 * 8)
+print(24 / 3)
---
+++ python_work/Chapter2/ex0210.py
@@ -0,0 +1,4 @@
+# Vairables, strings and numbers #
+fav_number = 14
+message = f"My favourite number is... drumroll.. {fav_number}!!!"
+print(message)
