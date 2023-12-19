44ef69a687409932c7a6a145e42ae4897caa306c
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-12-19 11:25:08 +0000

    Using Lists

---
+++ python_work/Chapter3/ex0301.py
@@ -0,0 +1,12 @@
+# Using Lists
+friends = ['john cena', 'big foot', 'john wick']
+print(f"Merry Christmas, {friends[0].title()}")
+print(f"Merry Christmas, {friends[1].title()}")
+print(f"Merry Christmas, {friends[-1].title()}")
+
+# Ex 0303
+cars = ['Buggati', 'BMW M5', 'Lambourghini Quntash', 'Ferrari La Ferrari']
+print(f"\n{cars[0]} made me not want to miss an espisode of Top Gear")
+print(f"{cars[1]} made me not want to miss an espisode of Top Gear")
+print(f"{cars[2]} made me not want to miss an espisode of Top Gear")
+print(f"{cars[3]} made me not want to miss an espisode of Top Gear")
