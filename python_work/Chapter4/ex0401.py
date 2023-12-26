87c2404fee64fe8ebc19c1f646aae09ec2048e62
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-12-26 22:38:28 +0000

    Looping through Lists

---
+++ python_work/Chapter4/ex0401.py
@@ -0,0 +1,6 @@
+# Working with For Loops
+pizzas = ['crisbreezy', 'kessame', 'chicken pepproni']
+for pizza in pizzas:
+    print(f"I love the {pizza} pizza, heaven to my taste buds\n")
+
+print("I just love pizza") #This statement is lie lol
---
+++ python_work/Chapter4/ex0402.py
@@ -0,0 +1,5 @@
+# Working with loops
+animals = ['dolphin', 'parrot', 'orcas']
+for animal in animals:
+    print(f"The {animal} is among the world's most intelligent animals")
+print("All these creatures are very intelligent")
