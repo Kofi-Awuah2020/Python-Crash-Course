a75c07309502127a8b2075a44134b36659907960
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-12-26 23:33:11 +0000

    List Comprehesion

--- python_tryouts/square_numbers.py
+++ python_tryouts/square_numbers.py
@@ -6,10 +6,14 @@ for value in range(1, 11): #store range 1 - 10 inclusive in value
     squares.append(square) #append result to the squares list
 
 print(squares)
-'''
+
 # Writing the code concisely
 squares = []
 for value in range(1, 11):
     squares.append(value ** 2)
 
+print(squares)
+'''
+#List Comprehension
+squares = [value ** 2 for value in range(1, 11)]
 print(squares)
