dcf79f1ff3bba40e19725aa1d0aa616144d0edfb
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-01 20:29:26 +0000

    Conditional Satements

---
+++ python_tryouts/cars.py
@@ -0,0 +1,7 @@
+# IF Stataments
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw': # Python uses columns C uses paranthesis
+        print(car.upper())
+    else:
+        print(car.title())
