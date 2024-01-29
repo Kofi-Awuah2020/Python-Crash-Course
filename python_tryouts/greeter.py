c9a5f02fd04a64b74477c7d563203c77de371b24
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-29 17:40:08 +0000

    functions

--- python_tryouts/greeter.py
+++ python_tryouts/greeter.py
@@ -1,9 +1,5 @@
-# Writing Clear Prompts
-name = input("Please enter your name: ")
-print(f"\nHello, {name}!")
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
 
-prompt = "If you share your name, we can personalize the messages you see."
-prompt += "\nWhat is your first name? "
-
-name = input(prompt)
-print(f"\nHello, {name}")
+greet_user('jack')
