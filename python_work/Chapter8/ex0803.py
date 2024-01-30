3e520d933ecb4408c4c3ea3b966a587efe79fa45
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-30 16:54:47 +0000

    Funcitons, defualt values, positional and keyword arguments

---
+++ python_work/Chapter8/ex0803.py
@@ -0,0 +1,8 @@
+def make_shirt(shirt_size='l', enscription='I love Python'):
+    print(f"\nShirt size: {shirt_size.upper()}, Text to be printed: {enscription}")
+
+make_shirt('xxl', 'God is the greatest')
+make_shirt(shirt_size='xxxl', enscription='Proud alpharian')
+make_shirt()
+make_shirt('m')
+make_shirt('s', 'I have backlog with ALX :(')
---
+++ python_work/Chapter8/ex0805.py
@@ -0,0 +1,6 @@
+def describe_city(city, country='Ghana'):
+    print(f"{city.title()} is in {country.title()}")
+
+describe_city('kumasi')
+describe_city('dakar', 'senegal')
+describe_city('koforidua')
