5e7c70bb305a4389126835e889ce54b0a96d0321
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-31 13:08:18 +0000

    Passing an arbitary number of arguments

---
+++ python_tryouts/make_pizza.py
@@ -0,0 +1,9 @@
+# Passing an Arbitary Number of Arguments
+def make_pizza(size, *toppings): # This would require variadic funcitons in C lol
+    """Summarize the pizza we are about to make."""
+    print(f"\nMaking a {size}-inch pizza with the following toppings:")
+    for topping in toppings:
+        print(f"- {topping}")
+
+make_pizza(16,'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
