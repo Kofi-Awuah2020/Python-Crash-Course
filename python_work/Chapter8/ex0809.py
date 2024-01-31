dc9546c217d6b8b268b8390095d1c510e25a36ad
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-31 12:32:54 +0000

    Passing a list to a function

---
+++ python_work/Chapter8/ex0809.py
@@ -0,0 +1,6 @@
+def show_messages(text_messages):
+    for text_message in text_messages:
+        print(f"\n{text_message}")
+
+short_texts =['lmao', 'omg', 'wllnh', 'lwkmd']
+show_messages(short_texts)
---
+++ python_work/Chapter8/ex0811.py
@@ -0,0 +1,11 @@
+def send_messages(text_messages, sent_messages):
+    while text_messages:
+        current_message = text_messages.pop()
+        print(f"\n{current_message}")
+        sent_messages.append(current_message)
+
+short_texts =['lmao', 'omg', 'wllnh', 'lwkmd']
+sent_messages = []
+send_messages(short_texts[:], sent_messages)
+print(short_texts)
+print(sent_messages)
