1322c2964b5645289bb4580b594c7450ddb7a7d4
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-05-31 06:12:18 +0000

    Battery upgrade method in Battery Class

--- python_tryouts/electric_car.py
+++ python_tryouts/electric_car.py
@@ -59,6 +59,10 @@ class Battery:
 
         print(f"This car can go about {range} miles on full charge.")
 
+    def upgrade_battery(self):
+        if self.battery_size != 65:
+            self.battery_size = 65
+
 class ElectricCar(Car):
     """Represent aspects of a car, specific to electric vehicles"""
 
@@ -77,4 +81,6 @@ class ElectricCar(Car):
 my_leaf = ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
 my_leaf.battery.describe_battery()
+my_leaf.battery.get_range()
+my_leaf.battery.upgrade_battery()
 my_leaf.battery.get_range()
