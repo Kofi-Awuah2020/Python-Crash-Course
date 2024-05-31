99d6c9885ba98817327ece888d32cbf0f1565ff1
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-05-31 07:11:20 +0000

    Importing an Entire Module

--- python_tryouts/my_cars.py
+++ python_tryouts/my_cars.py
@@ -1,6 +1,7 @@
-from car_module import Car, ElectricCar # type: ignore
+from car_module # type: ignore
 
-my_mustang = Car('ford', 'mustang', 2024)
+my_mustang = car.Car('ford', 'mustang', 2024)
 print(my_mustang.get_descriptive_name())
-my_leaf = ElectricCar('nissan', 'leaf', 2024)
+
+my_leaf = car.ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
