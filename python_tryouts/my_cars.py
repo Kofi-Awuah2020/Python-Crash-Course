2c4cde95e6ddfb38ebb223918cca4ef78413fa3c
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-05-31 09:23:54 +0000

    Using Aliases

--- python_tryouts/my_cars.py
+++ python_tryouts/my_cars.py
@@ -1,9 +1,9 @@
 from car_module import Car # type: ignore
-from electric_car import ElectricCar as EC # type: ignore
+import electric_car as ec # type: ignore
 
 
 my_mustang = Car('ford', 'mustang', 2024)
 print(my_mustang.get_descriptive_name())
 
-my_leaf = EC('nissan', 'leaf', 2024)
+my_leaf = ec.ElectricCar('nissan', 'leaf', 2024)
 print(my_leaf.get_descriptive_name())
