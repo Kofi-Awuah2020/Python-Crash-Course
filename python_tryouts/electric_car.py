008e038eb6dc6331a249b233d757b21c23bb0812
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-05-31 08:16:11 +0000

    Importing a module into a module

--- python_tryouts/electric_car.py
+++ python_tryouts/electric_car.py
@@ -1,43 +1,7 @@
 # Inheritance
-class Car:
-    """A simple attempt to represent a car."""
+"""A set of classes that can be used to represent electric cars."""
 
-    def __init__(self, make, model, year):
-        """Initialize atrributes to describe a car."""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        """Return a neatly formatted descriptive name."""
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-    
-    def read_odometer(self):
-        """Print a statement showing the car's mileage."""
-        print(f"This car has {self.odometer_reading} miles on it.")
-
-    def update_odometer(self, mileage):
-        """Set the odometer reading to the given value"""
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self, miles):
-        """Add the given amount to the odometer reading."""
-        self.odometer_reading += miles
-
-    def fill_gas_tank(self,fuel_guage):
-        """Fill gas take if empyty"""
-        if fuel_guage < 25:
-            print("Fuel at quarter! Fill the gas")
-        elif fuel_guage >= 50:
-            print("Fuel at half! Fill the gas")
-        elif 50 < fuel_guage <= 100:
-            print("Fuel almost full! Ride on")
-
+from car_module import Car # type: ignore
 
 class Battery:
     """A simple attempt to model a battery for an electric car."""
@@ -77,10 +41,3 @@ class ElectricCar(Car):
         """ELectric cars don't have gas tanks."""
         print("This car doesn't have a gas tank!")
 
-
-my_leaf = ElectricCar('nissan', 'leaf', 2024)
-print(my_leaf.get_descriptive_name())
-my_leaf.battery.describe_battery()
-my_leaf.battery.get_range()
-my_leaf.battery.upgrade_battery()
-my_leaf.battery.get_range()
