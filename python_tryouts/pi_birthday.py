b28f65d44a68eb64e5a982227729d35e4f0ce9bc
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-06-02 19:49:55 +0000

    making the code more concise

--- python_tryouts/pi_birthday.py
+++ python_tryouts/pi_birthday.py
@@ -3,9 +3,8 @@ from pathlib import Path
 path = Path('pi_million_digits.txt')
 contents = path.read_text()
 
-lines = contents.splitlines()
 pi_string = ''
-for line in lines:
+for line in contents.splitlines():
     pi_string += line.strip()
 
 birthday = input("Enter your birthday, in the form mmddyy: ")
