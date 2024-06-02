06dc78e33577dd34d2a92a97cb5745128195cbcc
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-06-02 19:44:49 +0000

    using the replace method

--- python_work/Chapter10/ex1001.py
+++ python_work/Chapter10/ex1001.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 path = Path('learning_python.txt')
 contents = path.read_text()
+contents = contents.replace('python', 'C')
 print(f"{contents}\n")
 
 lines = contents.splitlines()
